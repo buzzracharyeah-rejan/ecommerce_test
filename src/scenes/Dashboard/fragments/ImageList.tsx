124e607df492b67f58bcddf93fe72ac98d24391c
varbuzz <rejandev@gmail.com>
2023-07-06 15:58:44 +0545

    chore: setup iamge gallery

---
+++ src/scenes/Dashboard/fragments/ImageList.tsx
@@ -0,0 +1,62 @@
+import { Box, Container, ImageList, ImageListItem, Typography } from '@mui/material';
+import { itemData } from '../../../data';
+
+const ImageListings = () => {
+  return (
+    <Box display='flex' flexDirection='column' gap='1.5rem' m='3.5rem 1rem'>
+      <Typography
+        textAlign='center'
+        variant='h4'
+        sx={{ fontSize: '1.2rem', lineHeight: '150%', fontWeight: 600 }}
+      >
+        Share your setup with
+      </Typography>
+      <Typography
+        variant='h6'
+        component='h2'
+        textAlign='center'
+        sx={{ fontSize: '40px', fontStyle: 'normal', fontWeight: '700', lineHeight: '120%' }}
+      >
+        #FuniroFurniture
+      </Typography>
+      <Box
+        maxWidth='xl'
+        height='780px'
+        overflow='scroll'
+        display='flex'
+        justifyContent='center'
+        sx={{
+          '::-webkit-scrollbar': {
+            display: 'none',
+          },
+        }}
+      >
+        <ImageList
+          variant='masonry'
+          cols={3}
+          gap={8}
+          sx={{
+            '::-webkit-scrollbar': {
+              display: 'none',
+            },
+          }}
+        >
+          <Box width='451px' height='312px'>
+            {itemData.map((item) => (
+              <ImageListItem key={item.img}>
+                <img
+                  src={`${item.img}?w=161&fit=crop&auto=format`}
+                  srcSet={`${item.img}?w=161&fit=crop&auto=format&dpr=2 2x`}
+                  alt={item.title}
+                  loading='lazy'
+                />
+              </ImageListItem>
+            ))}
+          </Box>
+        </ImageList>{' '}
+      </Box>
+    </Box>
+  );
+};
+
+export default ImageListings;
--- src/scenes/Dashboard/index.tsx
+++ src/scenes/Dashboard/index.tsx
@@ -1,12 +1,14 @@
 import Banner from './fragments/Banner';
 import FeaturedProducts from './fragments/FeaturedProducts';
 import './dashboard.styles.css';
+import ImageListings from './fragments/ImageList';
 
 const Dashboard = () => {
   return (
     <>
       <Banner />
       <FeaturedProducts />
+      <ImageListings />
     </>
   );
 };
